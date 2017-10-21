d9842e437ab3501f3f7d5965e71e5e9d457c2dbe
Ignacio Tripodi <ignaciot@gmail.com>
2017-10-21 17:00:26 -0600

    Cleanup

--- analyze_results.py
+++ analyze_results.py
@@ -4,27 +4,31 @@ import operator
 
 from collections import Counter
 
-df = pd.read_csv('drug-drug-interactions.csv', header=1, sep=",", usecols=[1,2,4,5,7], names=['d1','p1','p_step','p2','d2'], dtype={'d1':'str', 'p1':'str', 'p_step':'str', 'p2':'str', 'd2':'str'})
+df = pd.read_csv('drug-drug-interactions-with-labels.csv', header=1, sep=",", \
+                 usecols=[1,2,4,5,7], names=['d1','p1','p_step','p2','d2'], \
+                 dtype={'d1':'str', 'p1':'str', 'p_step':'str', 'p2':'str', 'd2':'str'})
 
 # distinct reactome pathway steps
 p_steps = df.p_step.unique()
-print len(p_steps)
+print 'Distinct Reactome pathway steps: %d' % len(p_steps)
 
 # distinct drugs
 d1s = df.d1.unique()
 d2s = df.d2.unique()
 ds = np.concatenate((d1s, d2s))
-print len(set(ds))
+print 'Distinct drugs: %d' % len(set(ds))
 
 counts = Counter(df.p_step)
 
+print 'P450-related intersections:'
 nr_p450_steps = 0
 for key in counts.keys():
     if 'CYP1' in key or 'CYP2' in key or 'CYP3' in key or 'CYPI' in key or 'P450' in key:
         print "%s\t%s" % (key, counts[key])
         nr_p450_steps += counts[key]
-print nr_p450_steps
+print 'Total P450-related Reactome pathway steps: %d' % nr_p450_steps
 
+print 'All relation types with their number of occurrences:'
 sorted_pairs = sorted(counts.items(), key=operator.itemgetter(1), reverse=True)
 for pair in sorted_pairs:
     print '%s\t%s' % (pair[0], pair[1])
